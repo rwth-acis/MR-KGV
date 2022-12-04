fa8e99d34ca05733462e9b0d48f9b8ad7ba2fb83
Spitzkopf_Larry <alexander.rechtmann@rwth-aachen.de>
2022-12-04 12:47:04 +0100

    Added scripts to manage nodes and edges

---
+++ Assets/Scripts/Edge.cs
@@ -0,0 +1,19 @@
+// Script is attached to each edge element of the displayed graph.
+// It is used to manage the properties of the edge, such as
+// label, weight, object, subject, ...
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Edge : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,24 @@
+// Script is attached to each node element of the displayed graph.
+// It is used to manage the properties of the node, such as
+// label, annotation, representation type, ...
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    string label;
+    string annotation;
+    int representationType;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
